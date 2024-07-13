f9f47e9c38d9d404581e979f455844846c7c1885
OmphileMatsobe <omphilejmatsobe@gmail.com>
2024-07-13 21:31:50 +0200

    search and filter added

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,25 @@
 import Image from "next/image";
 import Hero from "./'components'/hero";
+import Search from "./'components'/search";
+import CustomFilter from "./'components'/filter";
 
 export default function Home() {
   return (
     <main className="overflow-hidden">
       <Hero/>
+      <div className="mt-12 padding-x padding-y max-width" id="discover">
+        <div className="home__text-container">
+          <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
+          <p>Explore the cars you might like</p>
+        </div>
+        <div className="home__filters">
+          <Search/>
+          <div className="home__filter-container">
+            <CustomFilter/>
+            <CustomFilter/>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
