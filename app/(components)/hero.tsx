82bf68ba0c5f300d6cae6f0bb0f7a90972a874a5
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-08-15 14:13:15 +0200

    App commit

--- app/(components)/hero.tsx
+++ app/(components)/hero.tsx
@@ -16,12 +16,11 @@ const Hero = () => {
     <div className="hero">
       <div className="flex-1 pt-36 padding-x">
         <h1 className="hero__title">
-          Find, book, rent a car—quick and super easy!
+          Discover, book, and rent a car in no time, hassle-free!
         </h1>
 
         <p className="hero__subtitle">
-          Streamline your car rental experience with our effortless booking
-          process.
+          Simplify your car rental with our easy booking process.
         </p>
 
         <CustomButton
--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,6 @@ export default async function Home() {
 
   return (
     <main className="overflow-hidden">
-      <Hero/>
       <div className="mt-12 padding-x padding-y max-width" id="discover">
         <div className="home__text-container">
           <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
